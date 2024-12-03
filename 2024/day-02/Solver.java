7d52e5ab918f6823d918492f974d1f84bef96c42
Mendel Debrabandere <mendel.debrabandere@ugent.be>
2024-12-03 23:45:45 +0100

    updated day 2 of 2024

--- 2024/day-02/Solver.java
+++ 2024/day-02/Solver.java
@@ -1,153 +1,91 @@
 import java.io.File;
-import java.util.Scanner;
 import java.io.FileNotFoundException;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Solver
-{
-    
+public class Solver {
     private Scanner myReader;
 
-    public Solver()
-    { }    
+    public Solver() {}
 
-    public void solve(boolean part1)
-    {
-        try
-        {
+    public void solve(boolean part1) {
+        try {
             File myObj = new File("input.txt");
             myReader = new Scanner(myObj);
             if (part1) {
-                solvePart1(myReader);
+                processInput(this::isSafeInPart1);
             } else {
-                solvePart2(myReader);
+                processInput(this::isSafeInPart2);
             }
-            myReader.close(); 
-        }
-        catch (FileNotFoundException e)
-        {
+            myReader.close();
+        } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
-    }    
-    
-    private void solvePart1(Scanner reader)
-    {
-        int safeAmount = 0;
-        
-        while (myReader.hasNextLine())
-        {
-            String line = myReader.nextLine();
-            System.out.println(line);
-            String[] strNumberArr = line.split(" ");
-            ArrayList<Integer> numbersArr = new ArrayList();
-            for (String strNumber : strNumberArr)
-            {
-                numbersArr.add(Integer.valueOf(strNumber));
-            }
-            
-            if (isLineSafe(numbersArr)) {
-                safeAmount += 1;
-            }
-        }
-        
-        System.out.println(safeAmount);
     }
-    
-    private void solvePart2(Scanner reader)
-    {
-        int safeAmount = 0;
-        
-        while (myReader.hasNextLine())
-        {
+
+    private void processInput(InputProcessor processor) {
+        int safeLines = 0;
+        while (myReader.hasNextLine()) {
             String line = myReader.nextLine();
-            System.out.println(line);
+            //System.out.println(line);
             String[] strNumberArr = line.split(" ");
-            ArrayList<Integer> numbersArr = new ArrayList();
-            for (String strNumber : strNumberArr)
-            {
+            ArrayList<Integer> numbersArr = new ArrayList<>();
+            for (String strNumber : strNumberArr) {
                 numbersArr.add(Integer.valueOf(strNumber));
             }
-            
-            boolean safeLine = isLineSafe(numbersArr);
-            
-            if (safeLine) {
-                safeAmount += 1;
+            safeLines += processor.process(numbersArr) ? 1 : 0;
+        }
+        System.out.println(safeLines);
+    }
+
+    private boolean isSafeInPart1(ArrayList<Integer> numbersArr) {
+        return isLineSafe(numbersArr);
+    }
+
+    private boolean isSafeInPart2(ArrayList<Integer> numbersArr) {
+        if (isLineSafe(numbersArr)) {
+            return true;
+        } else {
+            // Check if the line would be safe if we were to remove an item
+            boolean foundSafeLine = false;
+            for (int i = 0; i < numbersArr.size() && !foundSafeLine; ++i) {
+                ArrayList<Integer> numbersArrWithoutOne = new ArrayList<>(numbersArr);
+                numbersArrWithoutOne.remove(i);
+                if (isLineSafe(numbersArrWithoutOne)) {
+                    foundSafeLine = true;
+                }
+            }
+            if (foundSafeLine) return true;
+        }
+        return false;
+    }
+
+    private boolean isLineSafe(ArrayList<Integer> intArray) {
+        boolean increasing = true;
+        boolean firstNumber = true;
+        boolean secondNumber = true;
+        int previousNumber = -1;
+
+        for (Integer number : intArray) {
+            if (firstNumber) {
+                firstNumber = false;
             } else {
-                //check if the line would be safe if we were to remove an item
-                boolean foundSafeLine = false;
-                for (int i = 0; i < numbersArr.size() && !foundSafeLine; ++i)
-                {
-                    ArrayList<Integer> numbersArrWithoutOne = new ArrayList<>(numbersArr);
-                    numbersArrWithoutOne.remove(i);
-                    if (isLineSafe(numbersArrWithoutOne))
-                    {
-                        foundSafeLine = true;
-                    }
+                if (secondNumber) {
+                    secondNumber = false;
+                    increasing = number > previousNumber;
                 }
-                
-                if (foundSafeLine) safeAmount += 1;
-                
+
+                if (number == previousNumber || increasing != number > previousNumber) return false;
+                if (Math.abs(number - previousNumber) > 3) return false;
             }
-            
+            previousNumber = number;
         }
-        
-        System.out.println(safeAmount);
+        return true;
     }
-    
-    private boolean isLineSafe(ArrayList<Integer> intArray)
-    {      
-            boolean increasing = true;
-            boolean firstNumber = true;
-            boolean secondNumber = true;
-        
-            int previousNumber = -1;
-            
-            for (Integer number : intArray)
-            {
-                // System.out.println(numberString);
-                
-                if (firstNumber) {
-                    firstNumber = false;
-                } else {
-                    if (secondNumber) {
-                        secondNumber = false;
-                        increasing = number > previousNumber;
-                    }
-                    
-                    // unsafe if its the same number,
-                    // or if it isnt contiously increasing
-                    if (number == previousNumber || increasing != number > previousNumber) return false;
-        
-                    if (Math.abs(number - previousNumber) > 3) return false;
-                    
-                }
-                
-                previousNumber = number;
-            }
-            
-            return true;    
+
+    @FunctionalInterface
+    interface InputProcessor {
+        boolean process(ArrayList<Integer> numbersArr);
     }
-    
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
